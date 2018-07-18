811be1eff4dc9c5e0f778b0382ab7c2eedeb3cb7
Vladimir Romanovich <ibnteo@gmail.com>
2018-07-19 01:46:00 +0300

    remove f_auto_increment (deprecated), add f_insert_sequence

--- tarantoolib.lua
+++ tarantoolib.lua
@@ -25,7 +25,8 @@ function box.schema.space_mt.f_upsert(space, pk, data)
 end
 
 -- box.space.my:f_auto_increment({title='Abc', date=os.time()})
-function box.schema.space_mt.f_auto_increment(space, data)
+-- DEPRECATED
+--[[function box.schema.space_mt.f_auto_increment(space, data)
 	local f = {}
 	for k, v in pairs(space:format()) do
 		f[v.name] = k - 1
@@ -35,6 +36,21 @@ function box.schema.space_mt.f_auto_increment(space, data)
 		tupple[f[k] or k - 1] = v
 	end
 	return space:auto_increment(tupple)
+end]]--
+
+-- box.schema.sequence.create('my')
+-- box.space.my:create_index('pk', {sequence='my'}) or box.space.my.index.pk:alter({sequence='my'})
+-- box.space.my:f_insert_sequence({title='Abc', date=os.time()})
+function box.schema.space_mt.f_insert_sequence(space, data)
+	local f = {}
+	for k, v in pairs(space:format()) do
+		f[v.name] = k
+	end
+	local tupple = {box.NULL}
+	for k, v in pairs(data) do
+		tupple[f[k] or k] = v
+	end
+	return space:insert(tupple)
 end
 
 -- box.space.my:f_insert({id=1, title='Abc', date=os.time()})
